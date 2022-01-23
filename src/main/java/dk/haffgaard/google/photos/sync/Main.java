ebb8d4c578b384311ec5fc5b27916ceaf5c294d9
Jesper Langfeldt Hagen Haffgaard <jlh@systematic.com>
2022-01-23 13:42:24 +0100

    first commit

---
+++ src/main/java/dk/haffgaard/google/photos/sync/Main.java
@@ -0,0 +1,12 @@
+package dk.haffgaard.google.photos.sync;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.Credentials;
+import com.google.photos.library.v1.PhotosLibraryClient;
+
+public class Main {
+    public static void main(String[] args) {
+        Credentials credentials = FixedCredentialsProvider.create()
+    }
+}
