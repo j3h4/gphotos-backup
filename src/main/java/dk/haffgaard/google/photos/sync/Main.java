8ad46c3109adc1d02f20a2bf0ed29dead1552d36
Jesper Haffgaard <j3h4@github.com>
2022-07-08 10:36:34 +0200

    Minor changes

--- src/main/java/dk/haffgaard/google/photos/sync/Main.java
+++ src/main/java/dk/haffgaard/google/photos/sync/Main.java
@@ -2,11 +2,25 @@ package dk.haffgaard.google.photos.sync;
 
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.Credentials;
-import com.google.photos.library.v1.PhotosLibraryClient;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.photos.library.v1.PhotosLibrarySettings;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Credentials credentials = FixedCredentialsProvider.create()
+    public static void main(String[] args)  {
+        try {
+            CredentialsProvider credentialsProvider =
+                    FixedCredentialsProvider.create(
+                            GoogleCredentials.fromStream(new FileInputStream("credentials.json")));
+
+            PhotosLibrarySettings settings =
+                    PhotosLibrarySettings.newBuilder()
+                            .setCredentialsProvider(credentialsProvider)
+                            .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
